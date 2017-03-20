9fbc8b68210d9d8c899eb5db2004fcade9d132bf
Chloe Marshall <chloe.r.marshall@gmail.com>
2017-03-20 17:42:43 -0500

    Initial Commit

---
+++ Marshall_Project1_CSC389/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Marshall_Project1_CSC389
+//
+//  Created by Chloe Marshall on 3/20/17.
+//  Copyright © 2017 Chloe Marshall. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
